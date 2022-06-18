61c7e0412cc0a1c7777deb8702ed5503a5493683
Rodrigo Crispim <crispim1411@gmail.com>
2022-06-18 17:10:03 -0300

    padding

--- lib/main.dart
+++ lib/main.dart
@@ -22,28 +22,44 @@ class MyPage extends StatefulWidget {
 class _MyPage extends State<MyPage> {
   final List<String> _notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
   final Map<int, String> _selectedNotes = {};
-  int _rowsCounter = 1;
+  int _rowsCounter = 3;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.music_note),
-          title: const Text('Get the chord'),
-          backgroundColor: Colors.yellow[300],
-          foregroundColor: Colors.black,
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(20),
-          children: <Widget>[
-            // Notes
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: _rowsCounter,
-              itemBuilder: (context, index) => notesComboBox(index),
-            ),
-            // Add note row
-            Row(
+      appBar: AppBar(
+        leading: const Icon(Icons.music_note),
+        title: const Text('Get the chord'),
+        backgroundColor: Colors.yellow[300],
+        foregroundColor: Colors.black,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.restore),
+            padding: const EdgeInsets.only(right: 10),
+            onPressed: () {
+              setState(() {
+                _selectedNotes.clear();
+                _rowsCounter = 3;
+              });
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: <Widget>[
+          // Notes
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _rowsCounter,
+            itemBuilder: (context, index) => notesComboBox(index),
+          ),
+          // Padding
+          const Padding(padding: EdgeInsets.all(10)),
+          // Add note row, collapses if 4 notes
+          Visibility(
+            visible: _rowsCounter > 3 ? false : true,
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 GestureDetector(
@@ -61,27 +77,28 @@ class _MyPage extends State<MyPage> {
                 )
               ],
             ),
-            // Chord
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  child:
-                      const Icon(Icons.search, size: 30, color: Colors.black),
-                  onTap: () {
-                    setState(() {
-                      print('Search for chord with notes: $_selectedNotes');
-                    });
+          ),
+          // Padding
+          const Padding(padding: EdgeInsets.all(10)),
+          // Chord
+          OutlinedButton(
+            onPressed: _selectedNotes.length < 3
+                ? null
+                : () {
+                    print('Search for chord with notes: $_selectedNotes');
                   },
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text('Find the chord'),
-                )
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Icon(Icons.search, size: 30, color: Colors.black),
+                Padding(padding: EdgeInsets.all(10)),
+                Text('Find the chord'),
               ],
             ),
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 
   Widget notesComboBox(int index) {
