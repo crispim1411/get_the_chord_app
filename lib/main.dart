727855aba14d81ab936b5c13084b6c8fd85bca04
Rodrigo Crispim <crispim1411@gmail.com>
2022-06-21 02:02:42 -0300

    ajustes layout

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyPage extends State<MyPage> {
         foregroundColor: Colors.black,
         actions: [
           IconButton(
-            icon: const Icon(Icons.restore),
+            icon: const Icon(Icons.undo),
             padding: const EdgeInsets.only(right: 15),
             onPressed: restoreStatus,
           ),
@@ -70,7 +70,7 @@ class _MyPage extends State<MyPage> {
           visible: _rowsCounter > 3 ? false : true,
           child: addRowButton(),
         ),
-        const Padding(padding: EdgeInsets.all(10)),
+        const Padding(padding: EdgeInsets.all(5)),
         searchButton(),
         const Padding(padding: EdgeInsets.all(10)),
         Visibility(
@@ -102,10 +102,15 @@ class _MyPage extends State<MyPage> {
   }
 
   Widget searchButton() {
-    return Container(
-        height: 55,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-        child: OutlinedButton(
+    return Row(
+      children: [
+        const Spacer(),
+        Container(
+          height: 55,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: const Color.fromARGB(255, 230, 35, 35)),
             onPressed: _selectedNotes.length < 3
                 ? null
                 : () {
@@ -118,36 +123,45 @@ class _MyPage extends State<MyPage> {
                       _answer = Scale.getChord(notes.toList());
                     });
                   },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Icon(Icons.search, size: 30, color: Colors.black),
-                const Padding(padding: EdgeInsets.all(10)),
-                Text(
-                    _selectedNotes.length < 3
-                        ? 'Select the notes'
-                        : 'Find the chord',
-                    style: const TextStyle(fontSize: 18)),
-              ],
-            )));
+            child: Text(
+              _selectedNotes.length < 3
+                  ? 'Select the notes'
+                  : 'Search the chord',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
+        const Spacer()
+      ],
+    );
   }
 
   Widget answerBox() {
-    return Container(
-        height: 340 - _rowsCounter * 30,
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          border: Border.all(color: Colors.blueAccent),
-          borderRadius: BorderRadius.circular(50),
+    return Row(
+      children: [
+        const Spacer(),
+        Flexible(
+          flex: 15,
+          child: Container(
+            height: 350 - _rowsCounter * 20,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              border: Border.all(),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Text(
+              _answer,
+              style: TextStyle(
+                  fontSize: _answer.length > 10 ? 30 : 80,
+                  fontStyle: FontStyle.italic),
+            ),
+          ),
         ),
-        child: Text(
-          _answer,
-          style: TextStyle(
-              fontSize: _answer.length > 10 ? 30 : 100,
-              fontStyle: FontStyle.italic),
-        ));
+        const Spacer()
+      ],
+    );
   }
 
   Widget notesComboBox(int index) {
