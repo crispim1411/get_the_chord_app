7e861a46c11f3242eaa9b902cd3e53667792bd1e
Rodrigo Crispim <crispim1411@gmail.com>
2022-06-22 01:09:05 -0300

    enums models

---
+++ lib/models/enums.dart
@@ -0,0 +1,25 @@
+// Note { Symbol, Accidental }
+
+enum Symbol { C, D, E, F, G, A, B }
+
+enum Accidental { flat, normal, sharp }
+
+// Interval { Name, Type }
+
+enum IntervalName {
+  tonic,
+  second,
+  third,
+  fourth,
+  fifth,
+  sixth,
+  seventh,
+}
+
+enum IntervalType {
+  unison,
+  major,
+  minor,
+  perfect,
+  augmented,
+}
