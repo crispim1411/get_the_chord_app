090f4e43485e06bf64745cac9ee9691661aa105b
crispim <crispim1411@gmail.com>
2022-12-25 20:02:36 -0300

    group tets

--- test/widgets_test.dart
+++ test/widgets_test.dart
@@ -4,22 +4,24 @@ import 'package:get_chord_app/main.dart';
 import 'package:get_chord_app/form/form.dart';
 
 void main() {
-  testWidgets('load form page', (tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.byKey(const Key('FormPage')), findsOneWidget);
-  });
+  group('Page render tests', () {
+    testWidgets('load form page', (tester) async {
+      await tester.pumpWidget(const MyApp());
+      expect(find.byKey(const Key('FormPage')), findsOneWidget);
+    });
 
-  testWidgets('navigate to dict page', (tester) async {
-    await tester.pumpWidget(const MyApp());
-    await tester.tap(find.byKey(const Key('navDict')));
-    await tester.pumpAndSettle();
-    expect(find.byKey(const Key('DictPage')), findsOneWidget);
-  });
+    testWidgets('load dict page', (tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.tap(find.byKey(const Key('navDict')));
+      await tester.pumpAndSettle();
+      expect(find.byKey(const Key('DictPage')), findsOneWidget);
+    });
 
-  testWidgets('navigate to about page', (tester) async {
-    await tester.pumpWidget(const MyApp());
-    await tester.tap(find.byKey(const Key('navAbout')));
-    await tester.pumpAndSettle();
-    expect(find.byKey(const Key('AboutPage')), findsOneWidget);
+    testWidgets('load about page', (tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.tap(find.byKey(const Key('navAbout')));
+      await tester.pumpAndSettle();
+      expect(find.byKey(const Key('AboutPage')), findsOneWidget);
+    });
   });
 }
